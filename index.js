cbbe1bd7a3580ffb3e273447efcaea23e1a39d0c
Gavin McFarland <mindthetic@gavins-mbp.lan>
2018-10-03 20:51:11 +0100

    Before trying to creation single function

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import postcss from 'postcss';
 import { getProperties } from './lib/get-properties';
+import { updateProperties } from './lib/update-properties';
+// import { replaceValues } from './lib/methods';
 
 // const SIDES = /padding-top|padding-right|padding-bottom|padding-left/g;
+
 
 export default postcss.plugin('postcss-proportional-spacing', opts => {
 	console.log('opts', opts);
 
 	return (root) => {
 		root.walkRules(rule => {
-			const padding = getProperties(rule, "padding")
-			getProperties(rule, "margin")
-			console.log(padding);
+			const paddingProps = getProperties(rule, "padding");
+			updateProperties(paddingProps);
+			// const marginProps = getProperties(rule, "margin");
+			// replaceValues(rule, paddingProps, "padding");
+			// replaceValues(rule, marginProps, "margin");
 		});
 	};
 
