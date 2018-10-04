987ca7499d254cb6b7506f6db13fe9a699a76525
Gavin McFarland <mindthetic@gavins-mbp.lan>
2018-10-04 01:26:44 +0100

    Think calculations are working

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-import postcss from 'postcss';
-import { getProperties } from './lib/get-properties';
-import { updateProperties } from './lib/update-properties';
+import postcss from "postcss";
+import { getProperties, setValues } from "./lib/get-properties";
+// import { updateProperties } from './lib/update-properties';
 // import { replaceValues } from './lib/methods';
 
 // const SIDES = /padding-top|padding-right|padding-bottom|padding-left/g;
 
+export default postcss.plugin("postcss-proportional-spacing", opts => {
+  console.log("opts", opts);
 
-export default postcss.plugin('postcss-proportional-spacing', opts => {
-	console.log('opts', opts);
-
-	return (root) => {
-		root.walkRules(rule => {
-			const paddingProps = getProperties(rule, "padding");
-			updateProperties(paddingProps);
-			// const marginProps = getProperties(rule, "margin");
-			// replaceValues(rule, paddingProps, "padding");
-			// replaceValues(rule, marginProps, "margin");
-		});
-	};
-
-
+  return root => {
+    root.walkRules(rule => {
+      getProperties(rule, "padding");
+      setValues("padding");
+    });
+  };
 });
